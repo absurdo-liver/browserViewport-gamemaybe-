eaac4a657ac86cd8cec41d9be5540f053d671eea
absurdo-liver <6022537@cslaurentides.qc.ca>
2025-10-14 12:09:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,25 @@ const infoText = document.getElementById('infoText');
 const canvas = document.getElementById('cartesianCanvas');
 const ctx = canvas.getContext('2d');
 
-const clickedPoints = [];
+var clickedPoints = [];
+var lastMousePos = [];
+var distanceOrigin = 0;
+drawingExtras = false;
 
 window.addEventListener('mousemove', mouseMoveHandler);
+window.addEventListener('keydown', hotkeyHandler);
 canvas.addEventListener('click', clickHandler);
 
 
 function mouseMoveHandler(e){
   let centerX = resizeHandler()[0];
   let centerY = resizeHandler()[1];
-  let distanceOrigin = Math.round(Math.hypot(e.clientX - centerX, e.clientY - centerY)*1000)/1000;
+  distanceOrigin = Math.round(Math.hypot(e.clientX - centerX, e.clientY - centerY)*1000)/1000;
   infoText.textContent = `cursor: (${e.clientX}, ${e.clientY})
 center: (${centerX}, ${centerY})
 distance: ${distanceOrigin}px
 ` + vectorHandler([centerX,centerY],[e.clientX,e.clientY]);
+  lastMousePos = [e.clientX - centerX,e.clientY - centerY];
   renderCanvas(e.clientX - centerX,e.clientY - centerY);
 }
 
@@ -82,6 +87,44 @@ function renderCanvas(mousex, mousey) {
   ctx.lineTo(mousex, mousey);
   ctx.stroke();
 
+  // hotkey | draw extras
+  if(drawingExtras){
+    // circle
+    ctx.beginPath();
+    ctx.arc(0, 0, distanceOrigin, 0, 2 * Math.PI);
+    ctx.strokeStyle = "grey";
+    ctx.lineWidth = 3;
+    ctx.stroke();  
+    // vertical line
+    ctx.beginPath();
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 2;
+    ctx.moveTo(mousex, 0);
+    ctx.lineTo(mousex, mousey);
+    ctx.stroke();
+    // horizontal line
+    ctx.beginPath();
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 2;
+    ctx.moveTo(0, 0);
+    ctx.lineTo(mousex, 0);
+    ctx.stroke();
+
+  } else {return}
+}
+
+function hotkeyHandler(e){
+  if(e.key === 'x'){
+    clickedPoints = [];
+    renderCanvas(lastMousePos[0],lastMousePos[1]);
+  } else if(e.key === 'z'){
+    if(!drawingExtras){
+      drawingExtras = true;
+    } else {
+      drawingExtras = false;
+    }
+    renderCanvas(lastMousePos[0],lastMousePos[1]);
+  } else {return}
 }
 
 

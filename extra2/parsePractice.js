80ef6068f80e497ea36a4851d3e92ae5a747289f
absurdo-liver <6022537@cslaurentides.qc.ca>
2025-10-20 15:59:30 -0400

    Update parsePractice.js

--- extra2/parsePractice.js
+++ extra2/parsePractice.js
@@ -1,53 +1,37 @@
-function parsePartitionYInt(parts){
-  console.log(parts);
-  parts = parts.replaceAll(' ', ',');
-  parts = parts.split(',');
-  console.log(parts);
-  
-  if(parts[1] != '=') return 'must include "=" @ second place';
-  
+
+function parseTest(text){
+  if(typeof text != 'string') return 'enter a string'
   let output = {};
-  let part1 = parts[2];
-  let part2 = parts[4];
-  let independentVariable;
-  if(part1.includes('x')) independentVariable = 'x';
-  if(part1.includes('y')) independentVariable = 'y';
-  if(part1.includes('x') && part1.includes('y')){
-    console.log('invalid independent variable');
-    return
-  }
   
-  if(part1.length === 1 && part1 === independentVariable){
-    output.a = 1;
-  } else {
-    part1 = part1.replace(independentVariable, '');
-    if(part1.includes('.')) output.a = parseFloat(part1);
-    if(!part1.includes('.')) output.a = parseInt(part1);
-  }
+  output.text = text;
   
-  if(parts[3] === '+'){
-    if(part2.includes('.')) output.b = parseFloat(part2);
-    if(!part2.includes('.')) output.b = parseInt(part2);
-  } else if (parts[3] === '-'){
-    
-  } else {
-    console.log('invalid parameter before "b" variable');
-    return;
+  let words = text.split(' ');
+  for(let i = 0; i < words.length; i++){
+    if(words[i].includes('\'')){
+      let temp = words[i].split('\'');
+      let replacement;
+      if(temp[1] === 's'){
+        replacement = 'is';
+      }
+      if(temp[1] === 't'){
+        replacement = 'not';
+        if(temp[0] === 'didn') temp[0] = 'did';
+        if(temp[0] === 'wouldn') temp[0] = 'would';
+      }
+      if(temp[1] === 've'){
+        replacement = 'have';
+      }
+      
+      words.splice(i,1,temp[0],replacement);
+    } 
   }
+  output.words = words;
   
-  console.log(output);
+  
+  return output;
 }
 
+//let textToParse = 'what\'s going on ?';
+let textToParse = 'no i wouldn\'t'
 
-
-let YinterceptToParse = 'y = 3x + 1';
-parsePartitionYInt(YinterceptToParse);
-
-
-// let textToParse = 'y = ax^2 + bx + c';
-
-
-
-
-
-
+console.log(parseTest(textToParse));

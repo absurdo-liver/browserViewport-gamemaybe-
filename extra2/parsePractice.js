9ae8218b9ba104a7e515642684666671e78901a2
absurdo-liver <6022537@cslaurentides.qc.ca>
2025-10-21 09:14:26 -0400

    Update parsePractice.js

--- extra2/parsePractice.js
+++ extra2/parsePractice.js
@@ -1,36 +1,53 @@
-
-function parseTest(text){
-  if(typeof text != 'string') return 'enter a string'
-  let output = {};
-  
-  output.text = text;
-  
-  let words = text.split(' ');
-  for(let i = 0; i < words.length; i++){
-    if(words[i].includes('\'')){
-      let temp = words[i].split('\'');
-      let replacement;
-      if(temp[1] === 's'){
-        replacement = 'is';
-      }
-      if(temp[1] === 't'){
-        replacement = 'not';
-        if(temp[0] === 'didn') temp[0] = 'did';
-        if(temp[0] === 'wouldn') temp[0] = 'would';
-      }
-      if(temp[1] === 've'){
-        replacement = 'have';
-      }
-      
-      words.splice(i,1,temp[0],replacement);
-    } 
+function parseTest(text) {
+  if (typeof text !== 'string') {
+    return 'enter a string';
   }
-  output.words = words;
-  
-  
+
+  let output = {};
+  output.text = text;
+
+  const contractionMap = new Map([
+    ["'s", 'is'],
+    ["'ll", 'will'],
+    ["'re", 'are'],
+    ["'ve", 'have'],
+    ["'m", 'am'],
+    ["'d", 'would'],
+    ["n't", 'not'],
+  ]);
+
+  let words = text.split(' ');
+  let result = [];
+
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+
+    let replaced = false;
+    for (const [contraction, replacement] of contractionMap.entries()) {
+      if (word.endsWith(contraction)) {
+        let root = word.slice(0, word.length - contraction.length);
+        
+        if (word === "can't") {
+          result.push('can', 'not');
+        } else if (word === "won't") {
+          result.push('will', 'not');
+        } else {
+          result.push(root, replacement);
+        }
+        replaced = true;
+        break;
+      }
+    }
+
+    if (!replaced) {
+      result.push(word);
+    }
+  }
+
+  output.words = result;
   return output;
 }
 
-let textToParse = 'what\'s going on ?';
+let textToParse = "I'm sure you'll find what's wrong, but you can't blame me, I didn't see a thing.";
 
 console.log(parseTest(textToParse));

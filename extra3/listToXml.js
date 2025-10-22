588f6b50c040a85dd4243025ec766275671ff535
absurdo-liver <6022537@cslaurentides.qc.ca>
2025-10-22 11:55:15 -0400

    Update listToXml.js

--- extra3/listToXml.js
+++ extra3/listToXml.js
@@ -1,4 +1,51 @@
-const website = {}
+const website = {
+  html: {
+    head: {
+      title: {
+        text: 'nuh uhh',
+        id: 'none',
+        tags: {}
+      },
+      script: {
+        text: 'none',
+        id: 'none',
+        tags: {
+          src: 'script.js',
+          defer: true
+        }
+      },
+      link: {
+        text: 'none',
+        id: 'none',
+        tags: {
+          rel: 'stylesheet',
+          href: 'styles.css'
+        }
+      }
+    },
+    body: {
+      /*
+      <div id='info'>
+    <p id='mousePosCurrent'>(0, 0)</p>
+    <input id='rangeBar' type="range" min="1" max="100" value="7">
+    <br>
+    <p id='rangeValue'>7</p>
+    <br>
+    <button id='prettyCollectToggle'>Pretty Collect: off</button>
+    <br>
+    <p id='mouseClickCurrent'>up</p>
+    <p id='mouseWheelScroll'>1x [none]</p>
+    <p id='mouseIsMoving'>false</p>
+  </div>
+  <p id='cursorP'>+</p>
+  <p id='testElement'>test</p>
+  */
+    }
+  }
+}
+
+
+
 
 
 function objectToXml(obj) {

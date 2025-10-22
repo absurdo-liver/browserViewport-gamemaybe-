95aca225f23bd670a1b41a6e48674c5e2d140d31
absurdo-liver <6022537@cslaurentides.qc.ca>
2025-10-22 11:47:10 -0400

    Create listToXml.js

---
+++ extra3/listToXml.js
@@ -0,0 +1,20 @@
+const website = {}
+
+
+function objectToXml(obj) {
+  let xml = '';
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      const value = obj[key];
+      xml += `<${key}>`;
+      if (typeof value === 'object' && Object.keys(value).length > 0) {
+        xml += objectToXml(value);
+      }
+      xml += `</${key}>`;
+    }
+  }
+  return xml;
+}
+
+const xmlOutput = objectToXml(website);
+console.log(xmlOutput);
